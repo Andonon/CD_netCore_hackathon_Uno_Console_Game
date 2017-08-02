e57180c6a74fae48a837281077925af905ef119d
Troy Center <troycenter1@gmail.com>
2017-08-02 11:25:45 -0700

    imported deckofcards and working

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace uno
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Card jon = new Card("jon", "black", 1);
+            // Console.WriteLine(jon.val);
+            // Deck bob = new Deck().reset();
+            // Console.WriteLine(bob.cards.Count);
+            Deck troy = new Deck();
+            Player bill = new Player("Ricki");
+            Console.WriteLine(troy.cards.Count);
+
+            bill.draw(troy);
+            Console.WriteLine(bill.hand.Count);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            Console.WriteLine(bill.hand.Count);
+            bill.discard(4);
+            Console.WriteLine(bill.hand.Count);
+            
         }
     }
 }
